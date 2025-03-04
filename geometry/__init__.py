a7843de65495c51f55f53f26df40c06706211e69
Xiaolei Zhu <virtualzx@gmail.com>
2025-03-04 08:54:39 +0000

    Apply ruff auto-fixes to all modified files

--- geometry/__init__.py
+++ geometry/__init__.py
@@ -1,16 +1,16 @@
 
 """Geometry module containing various geometric shapes and algorithms."""
-from geometry.geometry import Circle, Rectangle
 from geometry.collision_detection import (
-    detect_circle_collision,
     detect_aabb_collision,
+    detect_circle_collision,
     detect_circle_rectangle_collision,
 )
+from geometry.geometry import Circle, Rectangle
 
 __all__ = [
     'Circle',
     'Rectangle',
-    'detect_circle_collision',
     'detect_aabb_collision',
+    'detect_circle_collision',
     'detect_circle_rectangle_collision',
 ]
